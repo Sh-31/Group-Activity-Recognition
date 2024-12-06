70a64b96d5ed0648b2e8646925c581ed8e68da5b
Sherif Ahmed <sherif201665@gmail.com>
2024-12-06 19:10:46 +0000

    add baseline 5

---
+++ modeling/baseline 5/model.py
@@ -0,0 +1,238 @@
+"""
+Baseline 5 Description :
+--------------------------------
+This baseline is a temporal extension of B3, 
+where person-specific features pooled over all individuals 
+in each frame are fed to an LSTM model 
+to capture group dynamics.
+"""
+import os
+import sys
+import torch
+import argparse
+import torch.nn as nn
+import albumentations as A
+import torchvision.models as models
+from albumentations.pytorch import ToTensorV2
+from torch.utils.data import DataLoader
+from torchinfo import summary
+
+class Person_Activity_Temporal_Classifer(nn.Module):
+    def __init__(self, num_classes, hidden_size, num_layers):
+        super(Person_Activity_Temporal_Classifer, self).__init__()
+        self.resnet50 = nn.Sequential(*list(models.resnet50(weights=models.ResNet50_Weights.DEFAULT).children())[:-1])
+        
+        self.lstm = nn.LSTM(
+                            input_size=2048,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=True,
+                        )
+
+        self.fc =  nn.Sequential(
+            nn.Linear(hidden_size, 32),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(32, 32),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(32, num_classes)
+        )
+    
+    def forward(self, x):
+        # x.shape => batch, bbox, frames, channals , hight, width
+        b, bb, seq, c, h, w = x.shape # seq => frames
+        x = x.view(b*bb*seq, c, h, w) # (batch * bbox * seq, c, h, w)
+        x = self.resnet50(x) # (batch * bbox * seq, 2048, 1 , 1)
+
+        x = x.view(b*bb, seq, -1) # (batch * bbox, seq, 2048)
+        x, (h , c) = self.lstm(x) # (batch * bbox, seq, hidden_size)
+
+        x = x[:, -1, :] # (batch * bbox, hidden_size)
+        x = self.fc(x)  # (batch * bbox, num_class)  
+        
+        return x
+
+class Group_Activity_Classifer(nn.Module):
+    def __init__(self, person_feature_extraction, num_classes):
+        super(Group_Activity_Classifer, self).__init__()
+
+        self.resnet50 = person_feature_extraction.resnet50
+        self.lstm = person_feature_extraction.lstm
+
+        for module in [self.resnet50,  self.lstm]:
+            for param in module.parameters():
+                param.requires_grad = False
+                
+        self.pool = nn.AdaptiveMaxPool2d((1, 2048))  # [Batch, 12, hidden_size] -> [Batch, 1, 2048]
+        
+        self.fc = nn.Sequential(
+            nn.Linear(2048, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, 128),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(128, num_classes), 
+        )
+    
+    def forward(self, x):
+        # x.shape => batch, bbox, frames, channals , hight, width
+        b, bb, seq, c, h, w = x.shape # seq => frames
+        x = x.view(b*bb*seq, c, h, w) # (b*bb*seq, c, h, w)
+        x = self.resnet50(x) # (batch * bbox * seq, 2048, 1 , 1)
+
+        x = x.view(b*bb, seq, -1) # (batch * bbox, seq, 2048)
+        x, (h , c) = self.lstm(x) # (batch * bbox, seq, hidden_size)
+        x = x[:, -1, :] # (batch * bbox, hidden_size)
+        
+        x = x.view(b, bb, -1) # (batch , bbox, hidden_size)
+        x = self.pool(x) # (batch, 1, 2048)
+        x = x.squeeze(dim=1) # (batch, 2048)
+
+        x = self.fc(x) # (batch, num_class)
+        return x
+
+def person_collate_fn(batch):
+    """
+    collate function to pad bounding boxes to 12 per frame and selecting the last frame label. 
+    """
+    clips, labels = zip(*batch)  
+    
+    max_bboxes = 12  
+    padded_clips = []
+    padded_labels = []
+
+    for clip, label in zip(clips, labels) :
+        num_bboxes = clip.size(0)
+        if num_bboxes < max_bboxes:
+            clip_padding = torch.zeros((max_bboxes - num_bboxes, clip.size(1), clip.size(2), clip.size(3), clip.size(4)))
+            label_padding = torch.zeros((max_bboxes - num_bboxes, label.size(1), label.size(2)))
+            
+            clip = torch.cat((clip, clip_padding), dim=0)
+            label = torch.cat((label, label_padding), dim=0)
+            
+        padded_clips.append(clip)
+        padded_labels.append(label)
+    
+    padded_clips = torch.stack(padded_clips)
+    padded_labels = torch.stack(padded_labels)
+    
+    padded_labels = padded_labels[:, :, -1, :]  # utile the label of last frame
+    b, bb, num_class = padded_labels.shape # batch, bbox, num_clases
+    padded_labels = padded_labels.view(b*bb, num_class)
+
+    return padded_clips, padded_labels
+
+def group_collate_fn(batch):
+    """
+    collate function to pad bounding boxes to 12 per frame and selecting the last frame label. 
+    """
+    clips, labels = zip(*batch)  
+    
+    max_bboxes = 12  
+    padded_clips = []
+
+    for clip in clips:
+        num_bboxes = clip.size(0)
+        if num_bboxes < max_bboxes:
+            clip_padding = torch.zeros((max_bboxes - num_bboxes, clip.size(1), clip.size(2), clip.size(3), clip.size(4)))
+            clip = torch.cat((clip, clip_padding), dim=0)
+    
+        padded_clips.append(clip)
+       
+    padded_clips = torch.stack(padded_clips)
+    labels = torch.stack(labels)
+    
+    labels = labels[:,-1, :]  # utile the label of last frame
+    
+    return padded_clips, labels
+
+# def eval(args, person_activity_checkpoint, checkpoint_path):
+
+#     sys.path.append(os.path.abspath(args.project_root))
+#     from helper_utils import load_config, load_checkpoint
+#     from eval_utils import model_eval
+#     from data_utils import Group_Activity_DataSet, group_activity_labels
+    
+#     config = load_config(args.config_path)
+
+#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#     person_activity_cls = Person_Activity_Classifer(num_classes=config.model['num_classes']['person_activity'])
+   
+#     person_activity_cls = load_checkpoint(model=person_activity_cls, checkpoint_path=person_activity_checkpoint, device=device, optimizer=None)
+    
+#     model = Group_Activity_ClassiferNN(person_feature_extraction=person_activity_cls, num_classes=config.model['num_classes']['group_activity'])
+
+#     model = load_checkpoint(model=model, checkpoint_path=checkpoint_path, device=device, optimizer=None)
+
+#     model = model.to(device)
+
+#     test_transforms = A.Compose([
+#         A.Resize(224, 224),
+#         A.Normalize(
+#             mean=[0.485, 0.456, 0.406],
+#             std=[0.229, 0.224, 0.225]
+#         ),
+#         ToTensorV2()
+#     ])
+    
+#     test_dataset = Group_Activity_DataSet(
+#         videos_path=f"{args.project_root}/{config.data['videos_path']}",
+#         annot_path=f"{args.project_root}/{config.data['annot_path']}",
+#         split=config.data['video_splits']['test'],
+#         labels=group_activity_labels, 
+#         transform=test_transforms,
+#         crops=True,
+#         seq=False, 
+#     )
+
+#     test_loader = DataLoader(
+#         test_dataset,
+#         batch_size=1,
+#         shuffle=True,
+#         num_workers=4,
+#         pin_memory=True
+#     )
+    
+#     criterion = nn.CrossEntropyLoss()
+   
+#     path = f"{args.project_root}/modeling/baseline 5/outputs"
+#     prefix = "Group Activity Baseline 5 eval on testset"
+
+#     metrics = model_eval(model=model, data_loader=test_loader, criterion=criterion, device=device , path=path, prefix=prefix, class_names=config.model["num_clases_label"]['group_activity'])
+
+#     return metrics
+
+if __name__ == "__main__":
+    ROOT = "/teamspace/studios/this_studio/Group-Activity-Recognition" 
+    MODEL_CONFIG = "/teamspace/studios/this_studio/Group-Activity-Recognition/modeling/configs/Baseline B5.yml"    
+    PERSON_ACTIVITY_CHECKPOINT_PATH = ""
+    GROUP_ACTIVITY_CHECKPOINT_PATH = ""
+   
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ROOT", type=str, default=ROOT,
+                        help="Path to the root directory of the project")
+    parser.add_argument("--config_path", type=str, default=MODEL_CONFIG,
+                        help="Path to the YAML configuration file")
+
+    args = parser.parse_args()
+    sys.path.append(os.path.abspath(args.ROOT))
+
+    from helper_utils import load_config
+    config = load_config(args.config_path)
+
+    person_act_cls = Person_Activity_Temporal_Classifer(
+        num_classes=config.model['num_classes']['person_activity'],
+        hidden_size=config.model['hyper_param']['person_activity']['hidden_size'],
+        num_layers=config.model['hyper_param']['person_activity']['num_layers']
+    )
+
+    model = Group_Activity_Classifer(
+        person_feature_extraction=person_act_cls, 
+        num_classes=config.model['num_classes']['group_activity']
+    )
+
+    # summary(model)
---
+++ modeling/baseline 5/trainer_b.py
@@ -0,0 +1,303 @@
+"""
+Phase two build group action classifier
+1. Get all player croups from traget frames (12, 3, H, W)
+1. Get player image representation (Person action classifier)
+3. MaxPool across all the players
+4. Build NN for group action classifier (using player pooled representation)
+"""
+
+import os
+import sys
+import yaml
+import torch
+import random
+import pickle
+import numpy as np
+import torch.nn as nn
+import albumentations as A
+import torch.optim as optim
+from datetime import datetime
+from albumentations.pytorch import ToTensorV2
+from torch.cuda.amp import autocast, GradScaler
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard.writer import SummaryWriter
+from model import Person_Activity_Temporal_Classifer, Group_Activity_Classifer, group_collate_fn
+
+ROOT = "/teamspace/studios/this_studio"
+PROJECT_ROOT= "/teamspace/studios/this_studio/Group-Activity-Recognition"
+CONFIG_FILE_PATH = ""
+PERSON_ACTIVITY_CHECKPOINT_PATH = ""
+
+sys.path.append(os.path.abspath(PROJECT_ROOT))
+
+from data_utils import Group_Activity_DataSet, group_activity_labels
+from eval_utils import get_f1_score, plot_confusion_matrix
+from helper_utils import load_config, setup_logging, load_checkpoint, save_checkpoint
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def train_one_epoch(model, train_loader, criterion, optimizer, scaler, device, epoch, writer, logger):
+    model.train()
+    total_loss = 0
+    correct = 0
+    total = 0
+    
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        
+        with autocast(dtype=torch.float16):
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+        
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        
+        total_loss += loss.item()
+        
+        predicted = outputs.argmax(1)
+        target_class = targets.argmax(1)
+        total += targets.size(0)
+        correct += predicted.eq(target_class).sum().item()
+        
+        if batch_idx % 100 == 0:
+            step = epoch * len(train_loader) + batch_idx
+            writer.add_scalar('Training/BatchLoss', loss.item(), step)
+            writer.add_scalar('Training/BatchAccuracy', 100.*correct/total, step)
+            
+            log_msg = f'Epoch: {epoch} | Batch: {batch_idx}/{len(train_loader)} | Loss: {loss.item():.4f} | Acc: {100.*correct/total:.2f}%'
+            logger.info(log_msg)
+    
+    epoch_loss = total_loss / len(train_loader)
+    epoch_acc = 100. * correct / total
+    
+    writer.add_scalar('Training/EpochLoss', epoch_loss, epoch)
+    writer.add_scalar('Training/EpochAccuracy', epoch_acc, epoch)
+    
+    return epoch_loss, epoch_acc
+
+def validate_model(model, val_loader, criterion, device, epoch, writer, logger, class_names):
+    model.eval()
+    total_loss = 0
+    correct = 0
+    total = 0
+    
+    y_true = []
+    y_pred = []
+    
+    with torch.no_grad():
+        for inputs, targets in val_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            
+            total_loss += loss.item()
+            
+            _, predicted = outputs.max(1)
+            _, target_class = targets.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(target_class).sum().item()
+            
+            y_true.extend(target_class.cpu().numpy())
+            y_pred.extend(predicted.cpu().numpy())
+    
+    avg_loss = total_loss / len(val_loader)
+    accuracy = 100. * correct / total
+    
+    f1_score = get_f1_score(y_true, y_pred, average="weighted")
+    writer.add_scalar('Validation/F1Score', f1_score, epoch)
+    
+    fig = plot_confusion_matrix(y_true, y_pred, class_names)
+    writer.add_figure('Validation/ConfusionMatrix', fig, epoch)
+    
+    writer.add_scalar('Validation/Loss', avg_loss, epoch)
+    writer.add_scalar('Validation/Accuracy', accuracy, epoch)
+    
+    logger.info(f"Epoch {epoch} | Valid Loss: {avg_loss:.4f} | Accuracy: {accuracy:.2f}% | F1 Score: {f1_score:.4f}")
+    
+    return avg_loss, accuracy
+
+def train_model(config_path, person_activity_checkpoint_path, checkpoint_path=None):
+   
+    config = load_config(config_path)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    person_act_cls = Person_Activity_Temporal_Classifer(
+        num_classes=config.model['num_classes']['person_activity'],
+        hidden_size=config.model['hyper_param']['person_activity']['hidden_size'],
+        num_layers=config.model['hyper_param']['person_activity']['num_layers']
+    )
+   
+    person_act_cls = load_checkpoint(
+        model=person_act_cls, 
+        checkpoint_path=person_activity_checkpoint_path, 
+        device=device, 
+        optimizer=None
+    )
+    
+    model = Group_Activity_Classifer(
+        person_feature_extraction=person_act_cls, 
+        num_classes=config.model['num_classes']['group_activity']
+    )
+    
+    model = model.to(device)
+   
+    optimizer = optim.AdamW(
+        model.parameters(),
+        lr=config.training['learning_rate'],
+        weight_decay=config.training['weight_decay']
+    )
+    
+    start_epoch = 0
+    best_val_acc = 0
+
+    if checkpoint_path:
+        model, optimizer, loaded_config, exp_dir, start_epoch = load_checkpoint(checkpoint_path, model, optimizer, device)
+        logger = setup_logging(exp_dir)
+    
+        if loaded_config:
+            config = loaded_config
+            logger.info(f"Resumed training from epoch {start_epoch}")
+            logger.info(f"Best validation accuracy so far: {best_val_acc:.2f}%")
+    else:
+         timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+       
+         exp_dir = os.path.join(
+                f"{PROJECT_ROOT}/modeling/baseline 5/{config.experiment['output_dir']}",
+                f"{config.experiment['name']}_V{config.experiment['version']}_{timestamp}"
+            )
+         
+         os.makedirs(exp_dir, exist_ok=True)
+         logger = setup_logging(exp_dir)
+
+    logger.info(f"Starting experiment: {config.experiment['name']}_V{config.experiment['version']}")
+    writer = SummaryWriter(log_dir=os.path.join(exp_dir, 'tensorboard'))
+    
+    logger.info(f"Using device: {device}")
+    
+    set_seed(config.experiment['seed'])
+    logger.info(f"Set random seed: {config.experiment['seed']}")
+    
+    train_transforms = A.Compose([
+        A.Resize(224, 224),
+        A.OneOf([
+            A.GaussianBlur(blur_limit=(3, 7)),
+            A.ColorJitter(brightness=0.2),
+            A.RandomBrightnessContrast(),
+            A.GaussNoise()
+        ], p=0.5),
+        A.OneOf([
+            A.HorizontalFlip(),
+            A.VerticalFlip(),
+        ], p=0.05),
+        A.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        ),
+        ToTensorV2()
+    ])
+
+    val_transforms = A.Compose([
+        A.Resize(224, 224),
+        A.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        ),
+        ToTensorV2()
+    ])
+
+    train_dataset = Group_Activity_DataSet(
+        videos_path=f"{PROJECT_ROOT}/{config.data['videos_path']}",
+        annot_path=f"{PROJECT_ROOT}/{config.data['annot_path']}",
+        split=config.data['video_splits']['train'],
+        labels=group_activity_labels,
+        transform=train_transforms,
+        crops=True,
+        seq=False, 
+    )
+    
+    val_dataset = Group_Activity_DataSet(
+        videos_path=f"{PROJECT_ROOT}/{config.data['videos_path']}",
+        annot_path=f"{PROJECT_ROOT}/{config.data['annot_path']}",
+        split=config.data['video_splits']['validation'],
+        labels=group_activity_labels,
+        transform=val_transforms,
+        crops=True,
+        seq=False, 
+    )
+    
+    logger.info(f"Training dataset size: {len(train_dataset)}")
+    logger.info(f"Validation dataset size: {len(val_dataset)}")
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=config.training['person_activity']['batch_size'],
+        collate_fn=group_collate_fn,
+        shuffle=True,
+        num_workers=4,
+        pin_memory=True
+    )
+    
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=config.training['person_activity']['batch_size'],
+        collate_fn=group_collate_fn,
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True
+    )
+    
+    criterion = nn.CrossEntropyLoss()
+    scaler = GradScaler()
+    
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer,
+        mode='min',
+        factor=0.1,
+        patience=5,
+    )
+    
+    config_save_path = os.path.join(exp_dir, 'config.yml')
+    with open(config_save_path, 'w') as f:
+        yaml.dump(config, f)
+    
+    logger.info("Starting training...")
+    for epoch in range(start_epoch, config.training['epochs']):
+        logger.info(f'\nEpoch {epoch+1}/{config.training["epochs"]}')
+        
+        train_loss, train_acc = train_one_epoch(
+            model, train_loader, criterion, optimizer, scaler, device, epoch, writer, logger
+        )
+        
+        val_loss, val_acc = validate_model(
+            model, val_loader, criterion, device, epoch, writer, logger, config.model['num_clases_label']
+        )
+        
+        scheduler.step(val_loss)
+        
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            save_checkpoint(model, optimizer, epoch, val_acc, config, exp_dir, is_best=True)
+
+        save_checkpoint(model, optimizer, epoch, val_acc, config, exp_dir)
+        
+        current_lr = optimizer.param_groups[0]['lr']
+        writer.add_scalar('Training/LearningRate', current_lr, epoch)
+        logger.info(f'Current learning rate: {current_lr}')
+    
+    writer.close()
+    
+    logger.info(f"Training completed.")
+
+if __name__ == "__main__":
+    # RESUME_CHECK_POINT  = ""
+    train_model(CONFIG_FILE_PATH, PERSON_ACTIVITY_CHECKPOINT_PATH)
